19df493c0ec0e26c87197f218f23936a1fef6710
Jonas Falck <jonas.falck@gmail.com>
2022-06-16 23:14:07 +0200

    TODO

--- read.go
+++ read.go
@@ -9,6 +9,9 @@ import (
 func ReadLongFrame(r io.Reader) (LongFrame, error) {
 	buf := bufio.NewReader(r)
 	// TODO must check the length of the frame since 0x16 can occur anywhere :S
+	// 68 LL LL 68
+	// The L field gives the quantity of the user data inputs plus 3 (for C,A,CI)
+	// AND add tests for this
 	msg, err := buf.ReadBytes(0x16)
 	if err != nil {
 		return nil, err
